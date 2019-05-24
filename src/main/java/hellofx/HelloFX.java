fa1411452f0e79af80a9b6181959a2ceb407a723
joonspoon <joonspoon@joonspoon.com>
2019-05-24 14:03:52 -0500

    Renamed ambiguous label.

--- src/main/java/hellofx/HelloFX.java
+++ src/main/java/hellofx/HelloFX.java
@@ -13,8 +13,8 @@ public class HelloFX extends Application {
     public void start(Stage stage) {
         String javaVersion = System.getProperty("java.version");
         String javafxVersion = System.getProperty("javafx.version");
-        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        Scene scene = new Scene(new StackPane(l), 640, 480);
+        Label helloLabel = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
+        Scene scene = new Scene(new StackPane(helloLabel), 640, 480);
         stage.setScene(scene);
         stage.show();
     }
